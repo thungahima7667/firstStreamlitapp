1003d75cd5e803d1c9934799a68e99d418b65060
thungahima7667 <75205907+thungahima7667@users.noreply.github.com>
2023-05-28 17:31:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,5 @@ stremlit.dataframe(fruits_to_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+import snowflake.connector
