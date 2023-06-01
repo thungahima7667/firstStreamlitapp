380e9ad227deae591fa6672b2fe16fb480005881
thungahima7667 <75205907+thungahima7667@users.noreply.github.com>
2023-06-01 19:12:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,5 @@ my_cur.execute("select*from fruit_load_list")
 my_data_rows= my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
+add_my_fruit = "JACK"
 
